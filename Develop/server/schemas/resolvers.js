41ffad8292582a8f5f1d2e28c358f6f46cbe6dcc
James <jamesdwire66@gmail.com>
2024-04-09 18:53:40 -0600

    fixed file structure

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,76 @@
+const { User } = require('../models');
+
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            console.log(context.user)
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                return userData;
+            }
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        // add users
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        // logins
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // saves the books
+    
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+        // removes books
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        }
+
+
+
+
+
+
+    },
+};
+
+module.exports = resolvers;
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # book-api-2
+
+## Description
+
+This is a simple API for managing a collection of books. It allows users to add, update, delete, and retrieve book information.
+
+## Installation
+
+Clone the repository and navigate to the project directory. Install the necessary dependencies with the following command:
