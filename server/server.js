2567c887d7ed82163b7470370c6f908de5a6ba10
James <jamesdwire66@gmail.com>
2024-04-13 15:09:28 -0600

    added console log

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const startApolloServer = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
+console.log('ping')
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
